22329a86d65e803d0ede2878dce294f34c05c265
Ro Han <rohank@valuefy.com>
2020-10-20 02:07:34 +0530

    added simulator func

--- core/game-simulator.js
+++ core/game-simulator.js
@@ -3,16 +3,36 @@
 const Card = require('./card');
 const Deck = require('./deck');
 
-let card = new Card(4, 7);
+const Game = require('./game');
 
-let deck = new Deck();
-console.log(deck.cards);
-console.log(deck.dealHand());
-console.log(deck.dealHand());
-console.log(deck.remainingCards());
-console.log(deck.cards);
+function simulateGame() {
+    let game = new Game();
 
-// console.log(card.getFaceValue());
+    let winner = null;
+    try {
+        winner = findWinner(game);
+        console.log('Winner:::', winner);
+    } catch (e) {
+        console.error(e);
+    }
 
-// // let deck = new Deck();
-// // console.log(dsseck);
+    console.log('End Game');
+}
+
+function findWinner(game) {
+    let winners = game.getWinners();
+
+    if (winners.length == 0) {
+        return new Error(`Invalid State`);
+    } else if (winners.length == 1) {
+        // Winner found;
+        return winner[0];
+    } else {
+        // There is a tie
+        game.playRound(winners);
+        return findWinner(game);
+    }
+
+}
+
+module.exports = simulateGame;
