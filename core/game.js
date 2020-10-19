a248da36bd98c8a870624f19771a7017b1360e1b
Ro Han <rohank@valuefy.com>
2020-10-20 01:51:23 +0530

    added game class

--- core/game.js
+++ core/game.js
@@ -1,12 +1,32 @@
 'use strict';
 
-const { DEFAULT_NUMBER_OF_PLAYERS } = require('./constants');
+const { DEFAULT_NUMBER_OF_PLAYERS, CARDS_IN_HAND } = require('./constants');
+const Hand = require('./hand');
 
 class Game {
     constructor(noOfPlayers) {
         noOfPlayers = noOfPlayers || DEFAULT_NUMBER_OF_PLAYERS;
-        this.players = [];
+        this.hands = new Array(noOfPlayers);
+        for (let i = 0; i < this.hands.length; i++) {
+            hands[i] = new Hand();
+        }
         this.deck = new Deck();
+        this.deal();
+    }
+
+    deal() {
+        let counter = CARDS_IN_HAND;
+        while (counter-- > 0) {
+            for (let hand in this.hands) {
+                // @TODO: Handle when card is null
+                let card = deck.dealCard();
+                hand.addCard(card);
+            }
+        }
+    }
+
+    getWinner() {
+        return [];
     }
 }
 
