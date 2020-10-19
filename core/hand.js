58c3b45d7d3f971af5e914cbeb774763467a69d3
Ro Han <rohank@valuefy.com>
2020-10-20 01:28:41 +0530

    added hand class

---
+++ core/hand.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class Hand {
+    constructor() {
+        this.cards = [];
+    }
+
+    addCard(card) {
+        this.cards.push(card);
+    }
+
+    score() {
+        // Score Calculator Logic
+        let score = 0;
+        return score;
+    }
+}
+
+module.exports = Hand;
