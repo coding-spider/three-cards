60a4862d3d75ed0fb222684550eaa7b482abfc26
Ro Han <rohank@valuefy.com>
2020-10-21 00:37:30 +0530

    done with score calculator

--- core/score-calculator.js
+++ core/score-calculator.js
@@ -19,9 +19,9 @@ class ScoreCalculator {
 }
 
 
-class Trail {
+class TrailScoreCalculagor {
     constructor() {
-        this.boost = 100;
+        this.boost = 100000;
         this.next = null;
     }
 
@@ -33,13 +33,74 @@ class Trail {
         let cards = hand.cards;
         if (cards.length === 3 && cards[0].value == cards[1].value && cards[1].value == cards[2].value) {
             return cards.reduce(function (sum, card) {
-                return sum + card.score;
+                return sum + (card.score * this.boost);
             }, 0);
         }
-        return thisb.next(hand);
+        return this.next(hand);
     }
 }
 
+
+class SequenceScoreCalculator {
+    constructor() {
+        this.boost = 10000;
+        this.next = null;
+    }
+
+    setNext(fn) {
+        this.next = fn;
+    }
+
+    calculate(hand) {
+        let cards = hand.cards;
+        if (cards.length === 3) {
+            cards.sort(function (a, b) { return a.value - b.value; });
+            if (cards[0].value - cards[1].value == -1 && cards[1].value - cards[2].value == -1) {
+                return cards.reduce(function (sum, card) {
+                    return sum + (card.score * this.boost);
+                }, 0);
+            }
+        }
+        return this.next(hand);
+    }
+}
+
+class PairScoreCalculator {
+    constructor() {
+        this.boost = 100;
+        this.next = null;
+    }
+
+    setNext(fn) {
+        this.next = fn;
+    }
+
+    calculate(hand) {
+        let cards = hand.cards;
+        if (cards.length === 3) {
+            if (cards[0].value == cards[1].value || cards[1].value == cards[2].value || cards[0].value == cards[2].value) {
+                return cards.reduce(function (sum, card) {
+                    return sum + (card.score * this.boost);
+                }, 0);
+            }
+        }
+        return this.next(hand);
+    }
+}
+
+class TopScoreCalculator {
+    constructor() {
+        this.boost = 1;
+        this.next = null;
+    }
+
+    calculate(hand) {
+        let cards = hand.cards;
+        return cards.reduce(function (sum, card) {
+            return sum + (card.score * this.boost);
+        }, 0);
+    }
+}
 
 
 
